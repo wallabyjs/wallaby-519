2a4dc79c81d53393abc15714fbe86879a13908b6
Artem Govorov <artem.govorov@gmail.com>
2016-03-18 15:17:34 +1000

    Updated wallaby-webpack, added d3-scale sample

--- app/a.spec.js
+++ app/a.spec.js
@@ -1,8 +1,13 @@
 import a from './a';
-import { expect } from 'chai'
+import {expect} from 'chai'
+
+import * as all from 'd3-scale'
+import {scaleBand, scalePoint} from 'd3-scale'
 
 describe('a', () => {
   it('should be ok', () => {
     expect(a.b).to.be.undefined
+    console.log(all);
+    console.log(scaleBand);
   })
 });
